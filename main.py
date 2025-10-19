d5542046cb500c22692b47423ad43dbd45ea0bd6
v.batsikadze <v.batsikadze@tnet.ge>
2025-10-19 12:06:50 +0400

    Debug image

--- main.py
+++ main.py
@@ -20,6 +20,10 @@ def detect_objects():
     try:
         data = request.json
         image_base64 = data['frame']
+        return jsonify({
+            'description': image_base64,
+            'objects': []
+        })
 
         # Remove data URL prefix if present
         if ',' in image_base64:
