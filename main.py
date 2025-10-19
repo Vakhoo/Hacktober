34425a70688e203dabaf429cc5faba0105a5b601
v.batsikadze <v.batsikadze@tnet.ge>
2025-10-19 18:05:51 +0400

    Experiment

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def detect_objects():
                 conf = box.conf[0].item()
                 cls = int(box.cls[0].item())
                 name = model.names[cls]
-                if name == "chair":
+                if name == "chair" or name == 'bus':
                     objects.append({
                         'name': name,
                         'confidence': conf,
@@ -59,6 +59,9 @@ def detect_objects():
                     })
 
                     description_string =  name
+
+                    if description_string == 'bus':
+                        description_string = 'table'
 
                     if description_string not in desc:
                         desc.append(description_string)
